a5d54f80c6cc15e47e1c0cd1f5a4b2b42bac05d0
Casey Rowley <crowley@instructure.com>
2020-02-03 06:24:12 -0700

    This commit demonstrates conditional functions in JSX

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,22 +1,45 @@
-'use strict';
+"use strict";
+
+/*
+Command to generate JSX file - babel src/app.js --out-file=public/scripts/app.js --presets=env,react --watch
+*/
 
 var app = {
     title: 'Indecision App',
-    subtitle: "This is the app!"
+    subtitle: "This is the app, yo!"
+};
+var user = {
+    name: "casey",
+    age: "Unknown",
+    location: "Wherever"
+};
+
+function getLocation(location) {
+    if (location) {
+        return location;
+    } else {
+        return "Unknown";
+    }
 };
 
 var template = React.createElement(
-    'div',
+    "div",
     null,
     React.createElement(
-        'h1',
+        "h1",
         null,
         app.title
     ),
     React.createElement(
-        'p',
+        "p",
         null,
         app.subtitle
+    ),
+    React.createElement(
+        "p",
+        null,
+        "Location: ",
+        getLocation(user.location)
     )
 );
 
--- src/app.js
+++ src/app.js
@@ -1,16 +1,32 @@
+/*
+Command to generate JSX file - babel src/app.js --out-file=public/scripts/app.js --presets=env,react --watch
+*/
 
 
 var app = {
     title: 'Indecision App',
-    subtitle: "This is the app!"
-}
+    subtitle: "This is the app, yo!"
+};
+var user = {
+    name: "casey",
+    age: "Unknown",
+    location: "Wherever"
+};
 
-
+function getLocation(location) {
+    if (location){
+        return location;
+    }
+    else{
+        return `Unknown`;
+    }
+};
 
 var template = (
     <div>
         <h1>{app.title}</h1>
         <p>{app.subtitle}</p>
+        <p>Location: {getLocation(user.location)}</p>
         
     </div>
     );
