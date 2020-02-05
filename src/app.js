a7d6ab7f0315df5f769cbff2da3a1e2537cadf53
CaseyInHaengsin <casey.rowley89@gmail.com>
2020-02-05 06:50:29 -0700

    Options and AddOption challengecompleted.

--- src/app.js
+++ src/app.js
@@ -1,14 +1,57 @@
 
 class Header extends React.Component {
     render(){
-        return <p>This is from Header</p>
+        return (
+            <div>
+                <h1>Indecision</h1>
+                <h2>What will you do?</h2>
+            </div>
+        );
     }
     
 }
 
+class Action extends React.Component {
+    render(){
+        return (
+            <div>
+                <button>What should I do? </button>
+            </div>
+        );
+    }
+}
+
+//Options -> Options component here
+class Options extends React.Component {
+    render(){
+        return (
+            <div>
+                <p>Here are your options!</p>
+            </div>
+        );
+    }
+}
+
+
+//AddOption -> 
+
+class AddOption extends React.Component {
+    render(){
+        return (
+            <div>
+                <p>Add an Option!</p>
+            </div>
+        );
+    }
+}
+
+
 const jsx = (
     <div>
-        <Header/>
+        <Header />
+        <Action />
+        <Options />
+        <AddOption />
     </div>
 );
 
