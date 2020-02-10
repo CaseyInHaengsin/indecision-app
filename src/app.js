796d072a6f1f0488a0def4767eae6c2b628f4e08
CaseyInHaengsin <casey.rowley89@gmail.com>
2020-02-10 06:46:39 -0700

    Learning to setup babel.

--- src/app.js
+++ src/app.js
@@ -1,1 +1,20 @@
-console.log('app.js is running');
+
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+const template = <p>This is JSX</p>;
+
+ReactDOM.render(template, document.getElementById('app'));
+
+
+//1. Installed babel-loader
+//2. Installed babel-core
+//3. Next, we went to the webpack.config.js file and added module to our exports
+//4. In webpack.config.js we added loader and test in an array object
+//5. We added excludes
+//6. We created the .babelrc file that we'll use to set presets.
+
+
+
